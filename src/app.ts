d617098a4b3dd9f455e3258cfa966677481fdd33
Juank628 <jsanchez_zunino@yahoo.com>
2023-03-09 23:00:25 -0500

    cors configured

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,18 @@
 import express from 'express';
+import cors from 'cors'
+import dotenv from 'dotenv';
 import { graphqlHTTP } from 'express-graphql';
 import { schema } from './schema';
 
+
 export const app = express();
+
+dotenv.config()
+const corsOptions = {
+    origin: process.env.CORS_URL,
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions))
 
 app.use('/graphql', graphqlHTTP({
     graphiql: true,
